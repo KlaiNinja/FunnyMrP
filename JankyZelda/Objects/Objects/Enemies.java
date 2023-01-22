5307deb2f36d39ad9b6bb245baf82e1496697f77
EdrisAd <117791134+EdrisAd@users.noreply.github.com>
2023-01-22 12:47:05 -0500

    Update Enemies.java

--- JankyZelda/Objects/Objects/Enemies.java
+++ JankyZelda/Objects/Objects/Enemies.java
@@ -1,5 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
+import java.util.*;
 /**
  * Write a description of class Enemies here.
  * 
@@ -8,25 +8,161 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Enemies extends Actor
 {
+    public static enum Dir {down,left,right,up}
+    static Dir kbDir = Dir.down; 
+    static Dir currentDir = Dir.down; 
     protected int health = 5; //hp of enemy
-    protected int maxHealth = 5;
     protected int atk; //amt of dmg deal to link
-    public int roomID;
+    protected int roomID;
     protected int xpos; //tracks x 
+    public int timer = 0;
     protected int ypos;// tracks y of each enemy
-    protected boolean alive = true; //alive?
-    public Enemies(int maxHealth, int atk, int roomID){
-        this.maxHealth = maxHealth;
-        this.atk = atk;
-        this.roomID = roomID;
-        reset();
+    protected boolean alive; //alive?
+    protected boolean isKnockback;
+    protected int counter = 0;
+    public void act(){
+        if(isKnockback && counter <= 5){
+            dirmove(kbDir, 15); //smooth kb not implemented yet
+            counter++;
+            if(counter == 4){
+                isKnockback = false;
+            } else {
+                counter = 0;
+            }
+        }
+        counter = 0;
     }
+
+    public void slowedmove(int mod){ //move slowly
+        if (timer%mod == 0){ //increase mod base to slow down the movement
+            move(1);
+        }
+    }
+
+    public void randomMove(){ //not implemented yet
+        Random generator = new Random(); //random number generator
+        int ranNum = generator.nextInt(250); //gets random number from 0-249
+        if(ranNum == 0){
+            currentDir = Dir.down;
+            
+        }
+        if (ranNum == 1){
+            currentDir = Dir.left;
+            
+        }
+        if(ranNum == 2){
+            currentDir = Dir.right;
+            
+        }
+        if(ranNum == 3){
+            currentDir = Dir.up;
+            
+        }
+    }
+
     public void takeDMG(int dmgtaken){
-        alive = health > 0;
-        if (alive){
+        if (health > 0){
             health -= dmgtaken;
         }else{
             destroy();
+        }
+    }
+
+    public void takeKB(Dir currentDirection){ // takes kb in stab direction
+        switch(currentDirection){
+            case up:
+                if(currentDir == Dir.up){
+                    kbDir = Dir.down;
+                } else {
+                    if (Link.stabDir == Link.Dir.down){
+                        kbDir = Dir.down;
+                    } else if (Link.stabDir == Link.Dir.up){
+                        kbDir = Dir.up;
+                    } else if (Link.stabDir == Link.Dir.left){
+                        kbDir = Dir.left;
+                    } else if (Link.stabDir == Link.Dir.right){
+                        kbDir = Dir.right;
+                    } else if (Link.stabDir == Link.Dir.down){
+                        kbDir = Dir.down;
+                    } 
+                }
+                break;
+            case down:
+                if(currentDir == Dir.down){
+                    kbDir = Dir.up;
+                } else {
+                    if (Link.stabDir == Link.Dir.down){
+                        kbDir = Dir.down;
+                    } else if (Link.stabDir == Link.Dir.up){
+                        kbDir = Dir.up;
+                    } else if (Link.stabDir == Link.Dir.left){
+                        kbDir = Dir.left;
+                    } else if (Link.stabDir == Link.Dir.right){
+                        kbDir = Dir.right;
+                    } else if (Link.stabDir == Link.Dir.down){
+                        kbDir = Dir.down;
+                    } 
+                }
+                break;
+            case left:
+                if(currentDir == Dir.left){
+                    kbDir = Dir.right;
+                } else {
+                    if (Link.stabDir == Link.Dir.down){
+                        kbDir = Dir.down;
+                    } else if (Link.stabDir == Link.Dir.up){
+                        kbDir = Dir.up;
+                    } else if (Link.stabDir == Link.Dir.left){
+                        kbDir = Dir.left;
+                    } else if (Link.stabDir == Link.Dir.right){
+                        kbDir = Dir.right;
+                    } else if (Link.stabDir == Link.Dir.down){
+                        kbDir = Dir.down;
+                    } 
+                }
+                break;
+            case right:
+                if(currentDir == Dir.right){
+                    kbDir = Dir.left;
+                } else {
+                    if (Link.stabDir == Link.Dir.down){
+                        kbDir = Dir.down;
+                    } else if (Link.stabDir == Link.Dir.up){
+                        kbDir = Dir.up;
+                    } else if (Link.stabDir == Link.Dir.left){
+                        kbDir = Dir.left;
+                    } else if (Link.stabDir == Link.Dir.right){
+                        kbDir = Dir.right;
+                    } else if (Link.stabDir == Link.Dir.down){
+                        kbDir = Dir.down;
+                    } 
+                }
+                break;
+        }
+
+    }
+
+    public void dirmove(Dir knockbackDirection, int speed){
+        System.out.println("checking");
+        int i = 0; 
+        switch(knockbackDirection){
+            case up:
+                i = speed;
+                //change kb for specific weapons of link here by changing i 
+                setLocation (getX(), getY() - i);
+                break;
+            case down:
+                i = speed;
+                setLocation (getX(), getY() + i);
+                break;
+            case left:
+                i = speed;
+                setLocation (getX() - i, getY());
+                break;
+            case right:
+                i = speed;
+                setLocation (getX() + i, getY());
+                break;
         }
     }
 
@@ -35,11 +171,12 @@ public class Enemies extends Actor
     }
 
     public void destroy(){
+        alive = false;
         removeSprite();
     }
 
     public void reset(){
-        this.health = maxHealth;
+        this.health = 5;
         this.alive  = true;
     }
 
@@ -51,8 +188,8 @@ public class Enemies extends Actor
         return this.atk;
     }
 
-    public void setAttk(int atk){
-        this.atk = atk;
+    public void setAttk(int attk){
+        this.atk = attk;
     }
 
     public void setHP(int hp){
@@ -61,12 +198,33 @@ public class Enemies extends Actor
             destroy();
         }
     }
-    public void takeKB(){
-        Actor Link = getWorld().getObjects(Link.class).get(0);
-        turnTowards(-Link.getX(), -Link.getY());
-        move(10);
+
+    public void getRot(int currentRot){
+        //default rotation when spawned is "sprite is looking towards right"
+        //left is 180, down is 90, right is 0, up is 270
+        if (0 < currentRot && currentRot < 90){
+            currentDir = Dir.right;
+        }
+        if (91 < currentRot && currentRot < 180){
+            currentDir = Dir.left;
+        }
+        if (181 < currentRot && currentRot < 270){
+            currentDir = Dir.up;
+        }
+        if (271 < currentRot && currentRot < 360){
+            currentDir = Dir.down;
+        }
     }
-    public boolean isAlive(){
-        return this.alive;
+
+    public void isHit(){ // this is old try to fix it
+        Actor link = getOneIntersectingObject(Link.class);
+        if(link != null){
+            if(Link.attacking == true){
+                if (getObjectsInRange(15, sword.class).size() > 0){
+                    this.takeDMG(1);
+                    this.takeKB(currentDir);
+                }
+            }
+        }       
     }
 }
