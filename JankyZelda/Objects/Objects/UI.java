1e50c86b0a11caa5878827e58aaa0c462ca178ba
EdrisAd <117791134+EdrisAd@users.noreply.github.com>
2023-01-22 17:58:13 -0500

    Create UI.java

---
+++ JankyZelda/Objects/Objects/UI.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class UI here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class UI extends Actor
+{
+    /**
+     * Act - do whatever the UI wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+       //nothing in here yet.
+      // just add some random ui things like a pause button and whatsoever
+      //could even just not use this, it could be useless
+    }
+}
