9458171216b51a79243ca9c17d820483937dc0d3
KlaiNinja <112980080+KlaiNinja@users.noreply.github.com>
2023-01-22 13:09:22 -0500

    Update Key.java

--- JankyZelda/Objects/Objects/Key.java
+++ JankyZelda/Objects/Objects/Key.java
@@ -7,5 +7,19 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @version (a version number or a date)
  */
 public class Key extends Actor{
-
+  public void act() 
+    {
+        // Add your action code here.
+        //if it is touching the player, then it can be picked up
+        if (isTouching(Link.class)){
+            playerInteraction(getWorld().getObjects(Link.class).get(0));
+        }
+    }
+    public void playerInteraction(Link player){
+        //when the player presses a button, it will pick up the key and remove it
+        if (Greenfoot.isKeyDown("E")){
+            player.foundKey();
+            getWorld().removeObject(this);
+        }
+    }
 }
