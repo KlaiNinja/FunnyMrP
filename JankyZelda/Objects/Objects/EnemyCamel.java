b170b1c2ab460475831d049eac6b7b603627961d
KlaiNinja <112980080+KlaiNinja@users.noreply.github.com>
2023-01-22 12:45:14 -0500

    Update EnemyCamel.java

--- JankyZelda/Objects/Objects/EnemyCamel.java
+++ JankyZelda/Objects/Objects/EnemyCamel.java
@@ -12,6 +12,9 @@ public class EnemyCamel extends Enemies
      * Act - do whatever the Enemy1 wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    public EnemyCamel(int maxHealth, int atk, int roomID){
+        super(maxHealth, atk, roomID);
+    }
     public void act()
     {
         // Add your action code here.
