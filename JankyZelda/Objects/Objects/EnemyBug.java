e9876be78e6f3690aabbf8f867253aee41af1c01
EdrisAd <117791134+EdrisAd@users.noreply.github.com>
2023-01-22 17:56:29 -0500

    Update EnemyBug.java

--- JankyZelda/Objects/Objects/EnemyBug.java
+++ JankyZelda/Objects/Objects/EnemyBug.java
@@ -8,53 +8,28 @@ import java.util.*;
  */
 public class EnemyBug extends Enemies
 {
-    int health = 3;
+    public EnemyBug(int maxHealth, int atk, int roomID){
+        super(maxHealth, atk, roomID);
+    }
+    int health = 5;
     int startX = 400;
     int startY = 400;
-    int speed;
-    public EnemyBug(int speed, int maxHealth, int atk, int roomID){
-        super(maxHealth, atk, roomID);
-        this.speed = speed;
-    }
     public void act()
     {
-        getRot( getRotation() );
-        rotate();
+        getDir( getRotation() );
+        currentDirToNEWS();
         Linkcheck();
         isHit();
     }
-
-    public void rotate(){ ///left is 180, down is 90, right is 0, up is 270
-        switch(currentDir){
-            case up:
-                setRotation(270);
-                break;
-            case down:
-                setRotation(90);
-                break;
-            case left:
-                setRotation(180);
-                break;
-            case right:
-                setRotation(0);
-                break;
-        }
-    }
     //This method moves the enemy like a homing missle as soon as it sees some player
-    public void Linkcheck()  {
+    public void Linkcheck(){
         timer++;
         Actor Link = getWorld().getObjects(Link.class).get(0);
-        if (!(getWorld().getObjects(Link.class).isEmpty() ) ){
-            if (getObjectsInRange(40*4, Link.class).size() > 0 ) {
-                turnTowards(Link.getX(), Link.getY());
-                slowedmove(2);
-            } else {
-                randomMove();
-            }
+        if (getObjectsInRange(40*4, Link.class).size() > 0 ) {
+            turnTowards(Link.getX(), Link.getY());
+            slowedmove(2);
         } else {
-            return;
+            this.randomMove();
         }
     }
-
-
 }
