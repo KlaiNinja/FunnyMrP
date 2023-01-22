74128de9328e063f4b5a04a0296de93c3be6b7dd
EdrisAd <117791134+EdrisAd@users.noreply.github.com>
2023-01-21 22:51:20 -0500

    Update EnemyBug.java

--- JankyZelda/Objects/Objects/EnemyBug.java
+++ JankyZelda/Objects/Objects/EnemyBug.java
@@ -8,31 +8,28 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class EnemyBug extends Enemies
 {
+    Collider collider;
     public static enum Dir {up,down,left,right}
-    Dir kbDir = Dir.down;
-    Dir currentDir = Dir.down;
+    public static Dir kbDir = Dir.down;
+    public static Dir currentDir = Dir.down;
     private int timer = 0;
     int health = 3;
-    public EnemyBug(){
-        //Enemies.setHP(5);   
-    }
-
     public void act()
     {
+        getRot(getRotation());
         Linkcheck();
     }
     //This method moves the enemy like a homing missle as soon as it sees some player
     public void Linkcheck()  {
         timer++;
         Actor Link = getWorld().getObjects(Link.class).get(0);
-
         if (!(getWorld().getObjects(Link.class).isEmpty())){
             if ( getObjectsInRange(40*4, Link.class).size() > 0 ) {
                 if (getObjectsInRange(50, Wall.class).size() >= 0 ) {
                     turnTowards(Link.getX(), Link.getY());
                     if (timer%2 == 0){ //increase mod base to slow down the movement
                         move(1);
-                    }
+                     }
                 }
             } 
         } else {
@@ -42,35 +39,25 @@ public class EnemyBug extends Enemies
             takeKB(); //knockback
         }
     }
-    public void takeKB(){
-        Actor Link = getWorld().getObjects(Link.class).get(0);
-        turnTowards(-Link.getX(), -Link.getY());
-        move(10);
+    
+    public void getRot(int currentRot){
+        //default rotation when spawned is "sprite is looking towards right"
+        //left is 180, down is 90, right is 0, up is 270
+        
+        if (0 < currentRot && currentRot < 90){
+            currentDir = Dir.right;
+        }
+        if (91 < currentRot && currentRot < 180){
+            currentDir = Dir.left;
+        }
+        if (181 < currentRot && currentRot < 270){
+            currentDir = Dir.up;
+        }
+        if (271 < currentRot && currentRot < 360){
+            currentDir = Dir.down;
+        }
+        if (Greenfoot.isKeyDown("space")){
+            System.out.println(currentRot);
+        }
     }
 }
-
-/*public void dirmove(Dir currentdir, int mvmtSpeed){
-int i = 0;
-switch(dir){
-case up:
-i = mvmtSpeed;
-//change kb for specific enemies here by changing i 
-setLocation (getX(), getY() - i);
-break;
-case down:
-i = mvmtSpeed;
-setLocation (getX(), getY() + i);
-break;
-case left:
-i = mvmtSpeed;
-setLocation (getX() - i, getY());
-break;
-case right:
-i = mvmtSpeed;
-setLocation (getX() + i, getY());
-break;
-}
-}
-
-}
- */
