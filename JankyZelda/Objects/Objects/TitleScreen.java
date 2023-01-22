f3cfc133969a8636efdc02ef840373f5e2343f05
Tonkotsu17 <122366945+Tonkotsu17@users.noreply.github.com>
2023-01-22 00:08:38 -0500

    Add files via upload

---
+++ JankyZelda/Objects/Objects/TitleScreen.java
@@ -0,0 +1,37 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class TitleScreen here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class TitleScreen extends World
+{
+
+    /**
+     * Constructor for objects of class TitleScreen.
+     * 
+     */
+    public TitleScreen()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(640, 480, 1); 
+        prepare();
+    }
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        TitleLetters titleletters = new TitleLetters();
+        addObject(titleletters, 320, 240);
+    }
+    
+    public void act()
+    {
+        if(Greenfoot.isKeyDown("enter"))
+        Greenfoot.setWorld(new RandomlyGeneratingDungeon());
+    }
+}
