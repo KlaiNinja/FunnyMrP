25a46b926d47273fe57d0bc2fad65f4a2ac33998
KlaiNinja <112980080+KlaiNinja@users.noreply.github.com>
2023-01-23 12:33:17 -0500

    Update Link.java

--- JankyZelda/Objects/Objects/Link.java
+++ JankyZelda/Objects/Objects/Link.java
@@ -2,37 +2,38 @@ import java.util.*;
 import greenfoot.*;
 
 public class Link extends Actor {
-    public static enum Dir {
-        up,down,left,right
-    }
-    public static Dir kbDir = Dir.down; //the knock back direction of link
-    public static Dir currentDir = Dir.down; //current direction of link
-    public static Dir stabDir = Dir.down; //the direction of the stabbing attack
+    public static enum Dir {up,down,left,right}
+    public static enum Attacks {Spin, Bow, Sword}
+    Class[] objects = {Wall.class,Block.class,Lava.class,Water.class, Door.class};
+    //knockback direction
+    Dir kbDir = Dir.down;
+    Dir currentDir = Dir.down;
+    Dir atkDir = Dir.down;
+    //the current active attack
+    Attacks activeAttack = Attacks.Spin;
+    //Link's weapons
+    Weapon[] weapons = new Weapon[3];
+    Weapon currentWeapon;
     int speed = 3; //rate of movement
     int xmove=0;
-    int xmove2=0;
     int ymove=0;
-    int ymove2=0;
     int scroll=0;
     int scrollTimer=0;
+    int angleRotation = 0;
     int roomID = 0;
     int HP = 10; 
-    int keysCollected = 0;
     int left, right, up, down;
+    public int spriteW, spriteH;
+    int keysCollected = 1;
+    boolean isKnockback = false;
+    boolean attacking = false;
     int knockbackDirection = 0;
     int counter = 0;
     int timer = 0;
-    int atktimer = 0;
-    public int spriteW;
-    public int spriteH;
-    GreenfootImage sprite;
-    boolean isKnockback = false;
-    public static boolean attacking = false;
-    boolean switchFrame = false;
-    boolean GameOver = false;
     Collider collider;
-    public GameOverScreen gos = new GameOverScreen();
-    public EndScreen endScreen = new EndScreen();
+    public static GameOverScreen gos = new GameOverScreen();
+    public static EndScreen endScreen = new EndScreen();
+    boolean switchFrame = false;
     GreenfootImage[] sprites = {
             new GreenfootImage("1LinkLeft.png"),
             new GreenfootImage("1LinkLeftMoving.png"),
@@ -43,12 +44,14 @@ public class Link extends Actor {
             new GreenfootImage("1LinkUp.png"),
             new GreenfootImage("1LinkUpMoving.png")
         };
-    public Link(int width,int height,int  colliderW,int  colliderH){
+    public Link(int width, int height, int colliderW, int colliderH){
+        //sets the collider and sprite it's width and height
         collider = new Collider(this, colliderW, colliderH);
         spriteW = width;
         spriteH = height;
+        //sets all the weapons of the player
+        weapons = new Weapon[]{new Sword(this, Attacks.Spin, 5, 5, 400, 200), new Sword(this, Attacks.Sword, 7, 1, 20, 100), new Bow(this, 3, 1, 5, 1)};
     }
-
     public void act() 
     {
         //Methods
@@ -56,13 +59,14 @@ public class Link extends Actor {
             try{
                 ((FadeOverlay)getWorld().getObjects(FadeOverlay.class).get(0)).setLocation(getX(),getY());
             }catch(IndexOutOfBoundsException e){}
-            if  (scroll == 0) {
+            if (scroll == 0){
                 basicMoving();
-                enemyHitCollision();
-                attack();
+                chooseAttack();
+                clearRoom();
+                enemyHitCollision(30);
                 playerAnimation(10);
                 if(isKnockback && counter <= 5){
-                    dirmove(kbDir, 14);
+                    dirMove(kbDir, 10);
                     counter++;
                     if(counter == 4){
                         isKnockback = false;
@@ -79,50 +83,46 @@ public class Link extends Actor {
                 if (getY()>=getWorld().getHeight()-20){scroll=3;}
                 if (getY()<=20){scroll=4;}
             }
-            //Press e to clear the current room after the player gets the key
-            if (Greenfoot.isKeyDown("E")){
-                clearRoom();
-            }
             if (scroll==0){
-                setLocation(getX()+xmove+xmove2,getY()+ymove+ymove2);
+                setLocation(getX()+xmove,getY()+ymove);
             }else{
                 scroll(5, 30);
             }
         } else {
-            //gameoverscreen once dead
-            Greenfoot.setWorld(new GameOverScreen1());
+            //death
+            //death();
         }
     }
 
-    public void dirmove(Dir dir,int  mvmtSpeed){
-        int  i = 0;
-        if (!(getObjectsInRange(50 + 14,Wall.class).size() > 0)){
-
-            switch(dir){
-                case up:
-                    i = mvmtSpeed;
-                    //change kb for specific enemies here by changing i 
-                    setLocation (getX(), getY() - i);
-                    break;
-                case down:
-                    i = mvmtSpeed;
-                    setLocation (getX(), getY() + i);
-                    break;
-                case left:
-                    i = mvmtSpeed;
-                    setLocation (getX() - i, getY());
-                    break;
-                case right:
-                    i = mvmtSpeed;
-                    setLocation (getX() + i, getY());
-                    break;
-            }
-        }else {
-
+    public void dirMove(Dir dir, int mvmtSpeed){
+        int i = 0;
+        switch(dir){
+            case up:
+                i = mvmtSpeed;
+                //change kb for specific enemies here by changing i 
+                setLocation (getX(), getY() - i);
+                break;
+            case down:
+                i = mvmtSpeed;
+                setLocation (getX(), getY() + i);
+                break;
+            case left:
+                i = mvmtSpeed;
+                setLocation (getX() - i, getY());
+                break;
+            case right:
+                i = mvmtSpeed;
+                setLocation (getX() + i, getY());
+                break;
         }
     }
 
-    public void scroll(int timeLength,int  speed){
+    public void death(){
+        //this will be more
+        getWorld().removeObject(this);
+    }
+
+    public void scroll(int timeLength, int speed){
         if (scrollTimer==0){((FadeOverlay)getWorld().getObjects(FadeOverlay.class).get(0)).fadeOut();}
         scrollTimer++;
         if(scrollTimer==timeLength){
@@ -160,54 +160,87 @@ public class Link extends Actor {
             }
         }
     }
-
-    public void attack(){
+    public void chooseAttack(){
         //p for spin, o for bow, and i for knife / sword
-        if (scroll!=0)
+        if (scroll!=0)return;
+        String key = Greenfoot.getKey();
+        if (attacking){
+            activateAttack();
             return;
-        if (Greenfoot.isKeyDown("p")){ 
-            attacking = true;
         }
-        if (Greenfoot.isKeyDown("space")){
-            stabAttack(currentDir);
-        }
-    }
-
-    public void stabAttack(Dir currentDirection){
-        switch(currentDirection){
-            case up:
-                stabDir = Dir.up;
-                currentDir = Dir.up;
+        if (key == null)return;
+        switch(key){
+            case "p":
                 attacking = true;
+                activeAttack = Attacks.Spin;
+                currentWeapon = weapons[0];
                 break;
-            case down:
-                stabDir = Dir.down;
-                currentDir = Dir.down;
+            case "o":
                 attacking = true;
+                activeAttack = Attacks.Sword;
+                currentWeapon = weapons[1];
                 break;
-            case left:
-                stabDir = Dir.left;
-                currentDir = Dir.left;
+            case "i":
                 attacking = true;
+                activeAttack = Attacks.Bow;
+                currentWeapon = weapons[2];
                 break;
-            case right:
-                stabDir = Dir.right;
-                currentDir = Dir.right;
-                attacking = true;
-                break;    
+            default:
+                break;
         }
     }
-
+    //activate the current weapon based on the chosen attack
+    public void activateAttack(){
+        if (activeAttack == Attacks.Spin && !currentWeapon.startCoolDown){
+            spinAttack(20);
+        }
+        //if the current weapon exists, then activate its attack
+        if (currentWeapon != null)currentWeapon.activateWeapon();
+    }
+    //Spin attack: rotates the player a full revolution and gives knockback to nearby enemies
+    public void spinAttack(int rotateSpeed){
+        List<Enemies> enemies = getObjectsInRange(55, Enemies.class);
+        if (angleRotation >= 359){
+            stopAttack();
+            angleRotation = 0;
+            currentWeapon.startCoolDown();
+        }else{
+            angleRotation += rotateSpeed;
+        }
+        //all enemies recieve knockback
+        for (Enemies enemy : enemies){
+            if (enemy != null){
+                enemy.takeKB(enemy.currentDir);
+            }
+        }
+        setRotation(angleRotation);
+    }
+    public void stopAttack(){
+        attacking = false;
+    }
+    public void stopMovement(){
+        xmove *= 0;
+        ymove *= 0;
+    }
+    //clears the room and open its doors after the player defeats all enemies
     public void clearRoom(){
         Map map = ((RandomlyGeneratingDungeon) getWorld()).map;
         Room currentRoom = map.getCurrentRoom();
-        Door[] doorsInRoom = currentRoom.getDoors() != null ? currentRoom.getDoors() : new Door[0];
-        currentRoom.unlockRoom(this, true);
+        //if all enemies are killed, then unlock the room and open the doors
+        System.out.println(currentRoom.getEnemies().size());
+        if (currentRoom.getEnemies() == null)return;
+        if (currentRoom.getEnemies().size() == 0){
+            System.out.println("Doors open");
+            currentRoom.unlockRoom(this, true);
+        }
+        else{
+            currentRoom.lockDoors();
+        }
     }
 
     public boolean isClose(int maxDist, Actor obj){
-        int  xDist = getX() - obj.getX();
-        int  yDist = getY() - obj.getY();
+        int xDist = getX() - obj.getX();
+        int yDist = getY() - obj.getY();
         double distance = Math.sqrt(xDist*xDist + yDist*yDist);
         return distance <= maxDist;
     }
@@ -217,14 +250,12 @@ public class Link extends Actor {
         System.out.println("Keys: " + keysCollected);
     }
 
-    public int  getKeysCollected(){
+    public int getKeysCollected(){
         return keysCollected;
     }
-
     public void basicMoving()
     {
-
-        if (scroll!=0)return;
+        if (scroll!=0 || (attacking && activeAttack == Attacks.Sword && currentWeapon.lockPlayerInPlace))return;
         left = Greenfoot.isKeyDown("a") ? 1 : 0;
         right = Greenfoot.isKeyDown("d") ? 1 : 0;
         up = Greenfoot.isKeyDown("w") ? 1 : 0;
@@ -248,7 +279,7 @@ public class Link extends Actor {
             currentDir = Dir.down;
         }
     }
-
+    //animates the player movement frame by frame at a specified framerate
     public void playerAnimation(int frameRate)
     {
         GreenfootImage frame1;
@@ -287,44 +318,40 @@ public class Link extends Actor {
             setImage(frame2);
         } 
     }
-    Class[] objects = {Wall.class,Block.class,Lava.class,Water.class, Door.class, Enemies.class};
-    int collisionAmount = 0;
-    //change this when adding boss !!!!1!11!11111!1!!1
-    public void enemyHitCollision(){
-        if (getObjectsInRange(30, Enemies.class).size() > 0){
+    //change this when adding boss
+    public void enemyHitCollision(int radius){
+        List<Enemies> nearbyEnemies = getObjectsInRange(radius, Enemies.class);
+        for (Enemies enemy : nearbyEnemies){
             if (isTouching(Enemies.class)){
-                initiateKB(currentDir); //everything does 1hp of dmg
+                initiateKB(enemy, currentDir); //everything does 1hp of dmg
             }
-        } 
-    }
-   public void initiateKB(Enemies Enemy, Dir currentDirection){
-        if (!(isTouching(Wall.class))){isKnockback = true;}else{isKnockback = false;}
-        if(HP == 4){
-            RandomlyGeneratingDungeon.heart5.removeSelf();
-        }else if(HP == 3){
-            RandomlyGeneratingDungeon.heart4.removeSelf();
-        }else if(HP == 2){
-            RandomlyGeneratingDungeon.heart3.removeSelf();
-        }else if(HP == 1){
-            RandomlyGeneratingDungeon.heart2.removeSelf();
-        }else if(HP == 0){
-            RandomlyGeneratingDungeon.heart1.removeSelf();
-            GameOver = true;
         }
-
-        takeDMG(1);
+    }
+     public void takeDMG(int amt){
+         if (HP > 0){
+            HP -= amt;
+        }
+        //do some thing else too
+    }
+    public void initiateKB(Enemies enemy, Dir currentDirection){
+        if (!(isTouching(Wall.class))){
+            isKnockback = true;
+        }else{
+            isKnockback = false;
+        }
+        enemy.takeDMG(1);
         switch(currentDirection){
             case up:
                 if(Greenfoot.isKeyDown("w")){
                     kbDir = Dir.down;
                 } else {
-                    if (Enemy.currentDir == Enemies.Dir.down){
+                    if (enemy.currentDir == Enemies.Dir.down){
                         kbDir = Dir.down;
-                    } else if (Enemy.currentDir == Enemies.Dir.up){
+                    } else if (enemy.currentDir == Enemies.Dir.up){
                         kbDir = Dir.up;
-                    } else if (Enemy.currentDir == Enemies.Dir.left){
+                    } else if (enemy.currentDir == Enemies.Dir.left){
                         kbDir = Dir.left;
-                    } else if (Enemy.currentDir == Enemies.Dir.right){
+                    } else if (enemy.currentDir == Enemies.Dir.right){
                         kbDir = Dir.right;
                     } 
                 }
@@ -333,13 +360,13 @@ public class Link extends Actor {
                 if(Greenfoot.isKeyDown("s")){
                     kbDir = Dir.up;
                 } else {
-                    if (Enemy.currentDir == Enemies.Dir.down){
+                    if (enemy.currentDir == Enemies.Dir.down){
                         kbDir = Dir.down;
-                    } else if (Enemy.currentDir == Enemies.Dir.up){
+                    } else if (enemy.currentDir == Enemies.Dir.up){
                         kbDir = Dir.up;
-                    } else if (Enemy.currentDir == Enemies.Dir.left){
+                    } else if (enemy.currentDir == Enemies.Dir.left){
                         kbDir = Dir.left;
-                    } else if (Enemy.currentDir == Enemies.Dir.right){
+                    } else if (enemy.currentDir == Enemies.Dir.right){
                         kbDir = Dir.right;
                     } 
                 }
@@ -348,13 +375,13 @@ public class Link extends Actor {
                 if(Greenfoot.isKeyDown("a")){
                     kbDir = Dir.right;
                 }else {
-                    if (Enemy.currentDir == Enemies.Dir.down){
+                    if (enemy.currentDir == Enemies.Dir.down){
                         kbDir = Dir.down;
-                    } else if (Enemy.currentDir == Enemies.Dir.up){
+                    } else if (enemy.currentDir == Enemies.Dir.up){
                         kbDir = Dir.up;
-                    } else if (Enemy.currentDir == Enemies.Dir.left){
+                    } else if (enemy.currentDir == Enemies.Dir.left){
                         kbDir = Dir.left;
-                    } else if (Enemy.currentDir == Enemies.Dir.right){
+                    } else if (enemy.currentDir == Enemies.Dir.right){
                         kbDir = Dir.right;
                     } 
                 }
@@ -363,26 +390,20 @@ public class Link extends Actor {
                 if(Greenfoot.isKeyDown("d")){
                     kbDir = Dir.left;
                 }else {
-                    if (Enemy.currentDir == Enemies.Dir.down){
+                    if (enemy.currentDir == Enemies.Dir.down){
                         kbDir = Dir.down;
-                    } else if (Enemy.currentDir == Enemies.Dir.up){
+                    } else if (enemy.currentDir == Enemies.Dir.up){
                         kbDir = Dir.up;
-                    } else if (Enemy.currentDir == Enemies.Dir.left){
+                    } else if (enemy.currentDir == Enemies.Dir.left){
                         kbDir = Dir.left;
-                    } else if (Enemy.currentDir == Enemies.Dir.right){
+                    } else if (enemy.currentDir == Enemies.Dir.right){
                         kbDir = Dir.right; 
                     }
                 }
                 break;
         }
     }
-
-    public void takeDMG(int amt){
-        HP = HP - amt; 
-        //do some thing else too
-    }
-
-    public Actor getObjectAtOffset(int dx, int  dy, Class object){
+    public Actor getObjectAtOffset(int dx, int dy, Class object){
         return getOneObjectAtOffset(dx, dy, object);
     }
 }
