323a339b2f4e54a20e48e23b6688ab18e4943ac0
Tonkotsu17 <122366945+Tonkotsu17@users.noreply.github.com>
2023-01-24 14:07:21 -0500

    Add files via upload

---
+++ GameOverScreen1.java
@@ -0,0 +1,26 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class GameOverScreen1 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class GameOverScreen1 extends World
+{
+    private int x;
+    /**
+     * Constructor for objects of class GameOverScreen1.
+     * 
+     */
+    public GameOverScreen1()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(640, 480, 1); 
+    }
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
---
+++ Zelda.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Zelda here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Zelda extends Actor
+{
+    /**
+     * Act - do whatever the Zelda wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
